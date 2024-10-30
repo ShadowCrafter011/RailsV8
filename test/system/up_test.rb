07b961c65b5f97041b05d13af2f8142836bf84da
ShadowCrafter011 <lkoe@bluewin.ch>
2024-10-30 09:31:48 +0000

    Testing Rails system tests on Github CI

--- test/system/up_test.rb
+++ test/system/up_test.rb
@@ -3,6 +3,6 @@ require "application_system_test_case"
 class UpTest < ApplicationSystemTestCase
     test "rails is up and healthy" do
         visit rails_health_check_url
-        assert_status :ok
+        assert_response :success
     end
 end
