5be4037ec1a44a7c378455ba2b1ab21713dcf0a8
ShadowCrafter011 <lkoe@bluewin.ch>
2024-10-30 10:52:14 +0000

    Set up system tests with remote browser in docker compose and integration test rails health check path

--- test/system/up_test.rb
+++
@@ -1,8 +0,0 @@
-require "application_system_test_case"
-
-class UpTest < ApplicationSystemTestCase
-    test "rails is up and healthy" do
-        visit rails_health_check_url
-        assert_response :success
-    end
-end
